a48914c5df92e47af7696c86df0d890b42fa6af9
kaien <kaienkira@gmail.com>
2022-02-18 15:35:23 +0800

    work sync

--- src/brickred/moment/base/class_util.h
+++ src/brickred/moment/base/class_util.h
@@ -23,4 +23,28 @@ private:                                \
     _T &operator=(const _T &) = delete; \
     _T &operator=(_T &&) = delete;      \
 
+#define BRICKRED_MOMENT_PRECREATED_SINGLETON(_T) \
+public:                                          \
+    static _T *getInstance()                     \
+    {                                            \
+        static _T obj;                           \
+        return &obj;                             \
+    }                                            \
+                                                 \
+private:                                         \
+    struct ObjectCreator {                       \
+        ObjectCreator() { _T::getInstance(); }   \
+    };                                           \
+    static ObjectCreator oc;                     \
+                                                 \
+    _T();                                        \
+    ~_T();                                       \
+    _T(const _T &) = delete;                     \
+    _T(_T &&) = delete;                          \
+    _T &operator=(const _T &) = delete;          \
+    _T &operator=(_T &&) = delete;               \
+
+#define BRICKRED_MOMENT_PRECREATED_SINGLETON_IMPL(_T) \
+    _T::ObjectCreator _T::oc;                         \
+
 #endif
---
+++ src/brickred/moment/base/internal_logger.h
@@ -0,0 +1,39 @@
+#ifndef BRICKRED_MOMENT_BASE_INTERNAL_LOGGER_H
+#define BRICKRED_MOMENT_BASE_INTERNAL_LOGGER_H
+
+#include <cstdarg>
+
+#include <brickred/moment/base/class_util.h>
+
+namespace brickred::moment::base {
+
+class InternalLogger {
+public:
+    struct LogLevel {
+        enum type {
+            MIN = 0,
+
+            DEBUG = 0,
+            WARNING,
+            ERROR,
+
+            MAX
+        };
+    };
+
+    using LogFunc = void (*)(int level, const char *format, va_list args);
+
+    void setLogFunc(LogFunc log_func);
+    void log(int level, const char *format, ...);
+
+private:
+    BRICKRED_PRECREATED_SINGLETON(InternalLogger)
+
+    LogFunc log_func_;
+};
+
+#define BRICKRED_MOMENT_LOG_DEBUG \
+    brickred::moment::base::InternalLogger::getInstance()->log( \
+        brickred::moment::base::InternalLogger::LogLevel::DEBUG, _format, ##__VA_ARGS__)
+
+} // namespace brickred::moment::base
