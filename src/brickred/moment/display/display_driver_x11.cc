52b8eea3390627770c6bc0250200f6c76ead69ce
kaien <kaienkira@gmail.com>
2022-04-17 17:21:13 +0800

    work sync

--- src/brickred/moment/display/display_driver_x11.cc
+++ src/brickred/moment/display/display_driver_x11.cc
@@ -1,5 +1,7 @@
 #include <brickred/moment/display/display_driver_x11.h>
 
+#include <unistd.h>
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <map>
 
@@ -37,22 +39,170 @@ WindowData::~WindowData()
 {
 }
 
+///////////////////////////////////////////////////////////////////////////////
+class DynamicLoadLibraryX11 {
+public:
+    using FN_XOpenDisplay = Display *(*)(
+        const char * // display_name
+    );
+    using FN_XCloseDisplay = int (*)(
+        Display * // display
+    );
+    using FN_XCreateColormap = Colormap (*)(
+        Display *, // display
+        Window,    // w
+        Visual *,  // visual
+        int        // alloc
+    );
+    using FN_XCreateWindow = Window (*)(
+        Display *,             // display
+        Window,                // parent
+        int,                   // x
+        int,                   // y
+        unsigned int,          // width
+        unsigned int,          // height
+        unsigned int,          // border_width
+        int,                   // depth
+        unsigned int,          // class
+        Visual *,              // visual
+        unsigned long,         // valuemask
+        XSetWindowAttributes * // attributes
+    );
+    using FN_XInternAtom = Atom (*)(
+        Display *,    // display
+        const char *, // atom_name
+        Bool          // only_if_exists
+    );
+    using FN_XChangeProperty = int (*)(
+        Display *,             // display
+        Window,                // w
+        Atom,                  // property
+        Atom,                  // type
+        int,                   // format
+        int,                   // mode
+        const unsigned char *, // data
+        int                    // nelements
+    );
+
+public:
+    DynamicLoadLibraryX11();
+    ~DynamicLoadLibraryX11();
+
+    bool load();
+    void unload();
+    bool checkLoadSuccess() const { return load_success_; }
+
+public:
+    FN_XOpenDisplay fn_x_open_display;
+    FN_XCloseDisplay fn_x_close_display;
+    FN_XCreateColormap fn_x_create_colormap;
+    FN_XCreateWindow fn_x_create_window;
+    FN_XInternAtom fn_x_intern_atom;
+    FN_XChangeProperty fn_x_change_property;
+
+private:
+    DynamicLoadLibrary dll_;
+    bool load_success_;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+DynamicLoadLibraryX11::DynamicLoadLibraryX11() :
+    fn_x_open_display(nullptr),
+    fn_x_close_display(nullptr),
+    fn_x_create_colormap(nullptr),
+    fn_x_create_window(nullptr),
+    fn_x_intern_atom(nullptr),
+    fn_x_change_property(nullptr),
+    load_success_(false)
+{
+}
+
+DynamicLoadLibraryX11::~DynamicLoadLibraryX11()
+{
+    unload();
+}
+
+bool DynamicLoadLibraryX11::load()
+{
+    if (dll_.load("libX11.so.6") == false) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to load libX11.so.6");
+        return false;
+    }
+
+    // XOpenDisplay
+    this->fn_x_open_display =
+        (FN_XOpenDisplay)dll_.findSymbol("XOpenDisplay");
+    if (nullptr == this->fn_x_open_display) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to find symbol XOpenDisplay in X lib");
+        return false;
+    }
+    // XCloseDisplay
+    this->fn_x_close_display =
+        (FN_XCloseDisplay)dll_.findSymbol("XCloseDisplay");
+    if (nullptr == this->fn_x_close_display) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to find symbol XCloseDisplay in X lib");
+        return false;
+    }
+    // XCreateColormap
+    this->fn_x_create_colormap =
+        (FN_XCreateColormap)dll_.findSymbol("XCreateColormap");
+    if (nullptr == this->fn_x_create_colormap) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to find symbol XCreateColormap in X lib");
+        return false;
+    }
+    // XCreateWindow
+    this->fn_x_create_window =
+        (FN_XCreateWindow)dll_.findSymbol("XCreateWindow");
+    if (nullptr == this->fn_x_create_window) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to find symbol XCreateWindow in X lib");
+        return false;
+    }
+    // XInternAtom
+    this->fn_x_intern_atom =
+        (FN_XInternAtom)dll_.findSymbol("XInternAtom");
+    if (nullptr == this->fn_x_intern_atom) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to find symbol XInternAtom in X lib");
+        return false;
+    }
+    // XChangeProperty
+    this->fn_x_change_property =
+        (FN_XChangeProperty)dll_.findSymbol("XChangeProperty");
+    if (nullptr == this->fn_x_change_property) {
+        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
+            "display_x11: failed to find symbol XChangeProperty in X lib");
+        return false;
+    }
+
+    load_success_ = true;
+
+    return true;
+}
+
+void DynamicLoadLibraryX11::unload()
+{
+    load_success_ = false;
+
+    this->fn_x_change_property = nullptr;
+    this->fn_x_intern_atom = nullptr;
+    this->fn_x_create_window = nullptr;
+    this->fn_x_create_colormap = nullptr;
+    this->fn_x_close_display = nullptr;
+    this->fn_x_open_display = nullptr;
+
+    dll_.unload();
+}
+
 } // namespace
 
 ///////////////////////////////////////////////////////////////////////////////
 class DisplayDriverX11::Impl {
 public:
-    using FN_XOpenDisplay = Display *(*)(const char *);
-    using FN_XCloseDisplay = int (*)(Display *);
-    using FN_XCreateColormap = Colormap (*)(
-        Display *, Window, Visual *, int);
-    using FN_XCreateWindow = Window (*)(
-        Display *, Window, int, int,
-        unsigned int, unsigned int, unsigned int, int, unsigned int,
-        Visual *, unsigned long, XSetWindowAttributes *);
-    using FN_XInternAtom = Atom (*)(
-        Display *, const char *, Bool);
-
     using WindowDataMap = std::map<int32_t, WindowData *>;
 
     Impl();
@@ -75,16 +225,7 @@ public:
     void pollEvents(bool block);
 
 private:
-    bool loadXlibDll();
-    void unloadXlibDll();
-
-private:
-    DynamicLoadLibrary x_lib_dll_;
-    FN_XOpenDisplay fn_x_open_display_;
-    FN_XCloseDisplay fn_x_close_display_;
-    FN_XCreateColormap fn_x_create_colormap_;
-    FN_XCreateWindow fn_x_create_window_;
-    FN_XInternAtom fn_x_intern_atom_;
+    DynamicLoadLibraryX11 x11_dll_;
 
     Display *display_;
     WindowDataMap windows_;
@@ -92,11 +233,6 @@ private:
 
 ///////////////////////////////////////////////////////////////////////////////
 DisplayDriverX11::Impl::Impl() :
-    fn_x_open_display_(nullptr),
-    fn_x_close_display_(nullptr),
-    fn_x_create_colormap_(nullptr),
-    fn_x_create_window_(nullptr),
-    fn_x_intern_atom_(nullptr),
     display_(nullptr)
 {
 }
@@ -108,7 +244,7 @@ DisplayDriverX11::Impl::~Impl()
 
 bool DisplayDriverX11::Impl::init()
 {
-    if (loadXlibDll() == false) {
+    if (x11_dll_.load() == false) {
         return false;
     }
 
@@ -119,16 +255,16 @@ void DisplayDriverX11::Impl::finalize()
 {
     disconnect();
 
-    unloadXlibDll();
+    x11_dll_.unload();
 }
 
 bool DisplayDriverX11::Impl::connect()
 {
-    if (nullptr == fn_x_open_display_) {
+    if (x11_dll_.checkLoadSuccess()) {
         return false;
     }
 
-    display_ = fn_x_open_display_(nullptr);
+    display_ = x11_dll_.fn_x_open_display(nullptr);
     if (nullptr == display_) {
         BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
             "display_x11: failed to open display");
@@ -140,10 +276,12 @@ bool DisplayDriverX11::Impl::connect()
 
 void DisplayDriverX11::Impl::disconnect()
 {
+    if (x11_dll_.checkLoadSuccess()) {
+        return;
+    }
+
     if (display_ != nullptr) {
-        if (fn_x_close_display_ != nullptr) {
-            fn_x_close_display_(display_);
-        }
+        x11_dll_.fn_x_close_display(display_);
         display_ = nullptr;
     }
 }
@@ -153,22 +291,24 @@ bool DisplayDriverX11::Impl::createWindow(
     int32_t pos_x, int32_t pos_y,
     uint32_t width, uint32_t height)
 {
-    if (nullptr == display_) {
+    if (x11_dll_.checkLoadSuccess()) {
         return false;
     }
-    if (nullptr == fn_x_create_colormap_ ||
-        nullptr == fn_x_create_window_) {
+
+    if (nullptr == display_) {
         return false;
     }
 
     int screen = DefaultScreen(display_);
+    Window root_window = RootWindow(display_, screen);
     Visual *visual = DefaultVisual(display_, screen);
     int depth = DefaultDepth(display_, screen);
-    Window root_window = RootWindow(display_, screen);
-    Colormap colormap = fn_x_create_colormap_(
-        display_, root_window, visual, AllocNone);
-    unsigned long value_mask = CWBorderPixel | CWColormap | CWEventMask;
 
+    // create colormap
+    Colormap colormap = x11_dll_.fn_x_create_colormap(
+        display_, root_window, visual, AllocNone);
+
+    // create window
     XSetWindowAttributes window_attrs = { 0 };
     window_attrs.colormap = colormap;
     window_attrs.background_pixel = 0xffffffff;
@@ -179,14 +319,24 @@ bool DisplayDriverX11::Impl::createWindow(
         KeyPressMask | KeyReleaseMask |
         ButtonPressMask | ButtonReleaseMask |
         EnterWindowMask | LeaveWindowMask | PointerMotionMask;
-
-    Window window_handler = fn_x_create_window_(
+    unsigned long value_mask = CWBorderPixel | CWColormap | CWEventMask;
+    Window window_handler = x11_dll_.fn_x_create_window(
         display_, root_window, pos_x, pos_y, width, height,
         0, depth, InputOutput, visual, value_mask, &window_attrs);
     if (!window_handler) {
         BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
             "display_x11: failed to create main window");
         return false;
+    }
+
+    // declare pid
+    {
+        const long pid = ::getpid();
+        Atom net_wm_pid = x11_dll_.fn_x_intern_atom(
+            display_, "_NET_WM_PID", False);
+        x11_dll_.fn_x_change_property(
+            display_, window_handler, net_wm_pid, XA_CARDINAL,
+            32, PropModeReplace, (unsigned char *)&pid, 1);
     }
 
     std::unique_ptr<WindowData> window(
@@ -218,70 +368,6 @@ void DisplayDriverX11::Impl::setWindowShouldClose(
 
 void DisplayDriverX11::Impl::pollEvents(bool block)
 {
-}
-
-///////////////////////////////////////////////////////////////////////////////
-bool DisplayDriverX11::Impl::loadXlibDll()
-{
-    if (x_lib_dll_.load("libX11.so.6") == false) {
-        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
-            "display_x11: failed to load libX11.so.6");
-        return false;
-    }
-
-    // XOpenDisplay
-    fn_x_open_display_ =
-        (FN_XOpenDisplay)x_lib_dll_.findSymbol("XOpenDisplay");
-    if (nullptr == fn_x_open_display_) {
-        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
-            "display_x11: failed to find symbol XOpenDisplay in X lib");
-        return false;
-    }
-    // XCloseDisplay
-    fn_x_close_display_ =
-        (FN_XCloseDisplay)x_lib_dll_.findSymbol("XCloseDisplay");
-    if (nullptr == fn_x_close_display_) {
-        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
-            "display_x11: failed to find symbol XCloseDisplay in X lib");
-        return false;
-    }
-    // XCreateColormap
-    fn_x_create_colormap_ =
-        (FN_XCreateColormap)x_lib_dll_.findSymbol("XCreateColormap");
-    if (nullptr == fn_x_create_colormap_) {
-        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
-            "display_x11: failed to find symbol XCreateColormap in X lib");
-        return false;
-    }
-    // XCreateWindow
-    fn_x_create_window_ =
-        (FN_XCreateWindow)x_lib_dll_.findSymbol("XCreateWindow");
-    if (nullptr == fn_x_create_window_) {
-        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
-            "display_x11: failed to find symbol XCreateWindow in X lib");
-        return false;
-    }
-    // XInternAtom
-    fn_x_intern_atom_ =
-        (FN_XInternAtom)x_lib_dll_.findSymbol("XInternAtom");
-    if (nullptr == fn_x_intern_atom_) {
-        BRICKRED_MOMENT_INTERNAL_LOG_ERROR(
-            "display_x11: failed to find symbol XInternAtom in X lib");
-        return false;
-    }
-
-    return true;
-}
-
-void DisplayDriverX11::Impl::unloadXlibDll()
-{
-    fn_x_intern_atom_ = nullptr;
-    fn_x_create_window_ = nullptr;
-    fn_x_create_colormap_ = nullptr;
-    fn_x_close_display_ = nullptr;
-    fn_x_open_display_ = nullptr;
-
-    x_lib_dll_.unload();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
